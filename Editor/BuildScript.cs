0d34576bfc65422be382753ec7d34ca97789cf83
Evgeny Shestakov <shesta7@yandex.ru>
2025-02-24 12:48:09 +0600

    fix build

--- Editor/BuildScript.cs
+++ Editor/BuildScript.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,10 +7,18 @@ public class BuildScript
     [MenuItem("Build/Perform Android Build")]
     public static void PerformBuild()
     {
-        string[] scenes = { "Assets/Scenes/Bootstrap.unity" }; // Replace with your scene paths
+        string[] scenes = GetScenesFromBuildSettings(); // Replace with your scene paths
         string buildPath = "build/android/game.apk";
 
         BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.Android, BuildOptions.None);
         Debug.Log("APK Build Completed!");
+    }
+
+    public static string[] GetScenesFromBuildSettings()
+    {
+        return EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path) // Return full scene path instead of just name
+            .ToArray();
     }
 }
